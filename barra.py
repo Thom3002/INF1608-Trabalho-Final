24500bb31686d61afbb699f0608e6dfc10fbd1bb
Marcello Nascif <marcellobnf@gmail.com>
2024-07-02 23:59:18 -0300

    Update barra.py

--- barra.py
+++ barra.py
@@ -3,7 +3,6 @@ class Barra:
         self._p1 = p1
         self._p2 = p2
         self.tam = tam
-        # self.tam = DistanciaParticulas(part1.getPosicao, part2.getPosicao)
 
     @property
     def p1(self):
